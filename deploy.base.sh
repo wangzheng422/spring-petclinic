bf908f4cecdae4ccadbcaaef84c17a4bbe8003fd
wzh <wangzheng422@gmail.com>
2018-04-18 19:24:10 +0800

    docker file remove intermedia

--- deploy.base.sh
+++ deploy.base.sh
@@ -5,7 +5,7 @@ set -x
 
 source config.sh
 
-docker build --force-rm -f docker/base.Dockerfile -t spring-petclinic:base ./
+docker build -f docker/base.Dockerfile -t spring-petclinic:base ./
 docker tag spring-petclinic:base ${REGISTRY}/spring-petclinic:base
 
 
--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ set -x
 
 source config.sh
 
-docker build --force-rm -f docker/mysql.Dockerfile -t spring-petclinic:mysql ./
+docker build -f docker/mysql.Dockerfile -t spring-petclinic:mysql ./
 docker tag spring-petclinic:mysql ${REGISTRY}/spring-petclinic:mysql
 
-docker build --force-rm -f docker/build.Dockerfile -t ${REGISTRY}/spring-petclinic ./
+docker build -f docker/build.Dockerfile -t ${REGISTRY}/spring-petclinic ./
 
 docker push ${REGISTRY}/spring-petclinic:base
 docker push ${REGISTRY}/spring-petclinic
