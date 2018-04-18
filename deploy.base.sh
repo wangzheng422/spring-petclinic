d3416963525f0e4bf9ba0fb980de3fefb87f68fb
wzh <wangzheng422@gmail.com>
2018-04-18 13:00:35 +0800

    deploy

--- deploy.base.sh
+++ deploy.base.sh
@@ -5,7 +5,7 @@ set -x
 
 source config.sh
 
-docker build -f docker/mysql.Dockerfile -t spring-petclinic:mysql ./
-docker tag spring-petclinic:mysql ${REGISTRY}/spring-petclinic:mysql
+docker build -f docker/base.Dockerfile -t spring-petclinic:base ./
+docker tag spring-petclinic:base ${REGISTRY}/spring-petclinic:base
 
 
--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ set -x
 
 source config.sh
 
-docker build -f docker/base.Dockerfile -t spring-petclinic:base ./
-docker tag spring-petclinic:base ${REGISTRY}/spring-petclinic:base
+docker build -f docker/mysql.Dockerfile -t spring-petclinic:mysql ./
+docker tag spring-petclinic:mysql ${REGISTRY}/spring-petclinic:mysql
 
 docker build -f docker/build.Dockerfile -t ${REGISTRY}/spring-petclinic ./
 
